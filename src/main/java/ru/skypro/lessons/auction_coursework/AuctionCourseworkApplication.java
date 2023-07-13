192ef13c83313d5e3945c47b91c50cfd9fe5ad72
ы <nvsemyonova@mail.ru>
2023-07-13 22:16:37 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/lessons/auction_coursework/AuctionCourseworkApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.auction_coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionCourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionCourseworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/lessons/auction_coursework/AuctionCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.auction_coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionCourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
